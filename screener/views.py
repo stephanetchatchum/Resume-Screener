4ba4caf491fb5450f4317311b011a3e44659a5c2
stephanetchatchum@gmail.com <stephanetchatchum@gmail.com>
2026-05-15 12:45:44 +0200

    importing Libraries

--- screener/views.py
+++ screener/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from .models import Job, Candidate
+from groq import Groq
+import PyPDF2
+import docx
+import io
 
 # Create your views here.
